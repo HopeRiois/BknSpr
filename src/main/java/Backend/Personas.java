ec280bdfac510bbaeb05f030feae849ea3eb65a0
Alvaro <alvaro2181705@correo.uis.edu.co>
2021-11-23 11:02:26 -0500

    Out

---
+++ src/main/java/Backend/Personas.java
@@ -0,0 +1,43 @@
+package Backend;
+
+import java.util.*;
+
+public class Personas {
+	private ArrayList<Persona> personas;
+	public Personas() {
+		this.personas= new ArrayList();
+	}
+	
+	public boolean agregarPersona(Persona persona){
+        return personas.add(persona);
+    }
+
+    public void removerPersona(int id){
+        personas.remove(id);
+    }
+
+    public void editarNombre(int id, String nombre){
+        personas.get(id).setNombre(nombre);
+    }
+
+    public void editarApellidos(int id, String apellidos){
+        personas.get(id).setApellidos(apellidos);
+    }
+
+    public void editarEdad(int id, int edad){
+        personas.get(id).setEdad(edad);
+    }
+
+    public Persona getPersona(int id){
+        return personas.get(id);
+    }
+
+    public String listar(){
+        String datos ="";
+        for(Persona i : personas)
+        {
+            datos= datos + i.getNombre() + " " + i.getApellidos() + " " + i.getEdad() + "\n";
+        }
+        return datos;
+    }
+}
