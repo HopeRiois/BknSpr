e323b59929067124b570579bc35afe415c9628bb
Alvaro Rios <alvaroluisriosgarzon@gmail.com>
2021-11-25 10:59:12 -0500

    PersonaConstructorAuto

--- Backend/src/main/java/Backend/Personas.java
+++ Backend/src/main/java/Backend/Personas.java
@@ -6,6 +6,8 @@ public class Personas {
 	private ArrayList<Persona> personas;
 	public Personas() {
 		this.personas= new ArrayList();
+        Persona p = new persona(1,XX,YY,100);
+        personas.add(p);
 	}
 	
 	public boolean agregarPersona(Persona persona){
