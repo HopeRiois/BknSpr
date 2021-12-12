c95f690e22f1003c20d7398372916eff79fc377f
Alvaro Rios <alvaroluisriosgarzon@gmail.com>
2021-12-12 08:52:33 -0500

    BugFix

--- src/main/java/Backend/BackendApplication.java
+++ src/main/java/Backend/BackendApplication.java
@@ -15,14 +15,55 @@ public class BackendApplication {
 	}
 	
 	@GetMapping("/add")
-	public String hello(@RequestParam(value = "ID") int id,
-	@RequestParam(value = "name") String name, 
+	public String Agregar(@RequestParam(value = "name") String name, 
 	@RequestParam(value = "apellidos") String apellidos,
 	@RequestParam(value = "edad") int edad)
 	{
 		Personas p = new Personas();
-		Persona person = new Persona(id,name,apellidos,edad);
-		p.agregarPersona(person);
+		String prueba = "datos antes: " + p.listar();
+		Persona person2 = new Persona(4,name,apellidos,edad);
+		p.agregarPersona(person2);
+		prueba = prueba + "\n" + " datos después: " + p.listar();
+		return String.format(prueba);
+	}
+
+	@GetMapping("/getAll")
+	public String Listar()
+	{
+		Personas p = new Personas();
 		return String.format(p.listar());
+	}
+
+	@GetMapping("/getOne")
+	public String GetPersona(@RequestParam(value = "ID") int id)
+	{
+		Personas p = new Personas();
+		return String.format(p.getPersona(id));
+	}
+
+	@GetMapping("/edit")
+	public String EditPersona(@RequestParam(value = "ID") int id,
+								@RequestParam(value = "nombre") String nombre,
+								@RequestParam(value = "apellidos") String apellidos,
+								@RequestParam(value = "edad") int edad)
+	{
+		Personas p = new Personas();
+		String prueba = "datos antes: " + p.listar();
+		p.editarApellidos(id, apellidos);
+		p.editarNombre(id, nombre);
+		p.editarEdad(id, edad);
+
+		prueba = prueba + "\n" + " datos después: " + p.listar();
+		return String.format(prueba);
+	}
+
+	@GetMapping("/delete")
+	public String DeletePersona(@RequestParam(value = "ID") int id)
+	{
+		Personas p = new Personas();
+		String prueba = "datos antes: " + p.listar();
+		p.removerPersona(id);
+		prueba = prueba + "\n" + " datos después: " + p.listar();
+		return String.format(prueba);
 	}
 }
